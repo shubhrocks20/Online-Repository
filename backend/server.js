cd07dd91317eb3b48fa11e0aba8bb5c9871b7254
Shubham kumar <shubhamkumar2056@gmail.com>
2024-03-19 01:02:03 +0530

    Third

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 app.use('/api', router);
+app.get('/', (req, res)=>{
+    res.json('OK')
+})
 mongoose.connect(`${process.env.MONGO_URI}`)
 .then(()=>console.log(`Mongo Db Connected Successfully!`))
 .catch((err)=>console.error('Error while Connecting to Mongo:', err))
