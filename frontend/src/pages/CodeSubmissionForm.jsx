9cae7991f91e9431fd124d69895e38c386a4d0f9
Shubham kumar <shubhamkumar2056@gmail.com>
2024-03-19 20:03:44 +0530

    UI Updated

--- frontend/src/pages/CodeSubmissionForm.jsx
+++ frontend/src/pages/CodeSubmissionForm.jsx
@@ -44,72 +44,69 @@ const CodeSubmissionForm = () => {
       setExecutionResult(response.data.output || '');
       setExecutionError(response.data.error || '');
     } catch (error) {
-      
       setExecutionError(error.response.data.error.message);
     }
   };
 
   return (
-    <div className="container mx-auto py-2 px-8 animate-fade-in-up">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 hover:scale-105" onClick={() => { navigate('/submissions') }}>🔙 Submission Table</button>
-      <h1 className="font-bold text-4xl text-center mt-8 mb-6">Code Submission Form</h1>
-      <div className="max-w-lg mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        <form onSubmit={handleSubmit} className="bg-white p-8 animate-fade-in-down">
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username:</label>
-            <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+    <div className="container mx-auto p-6 bg-gray-100 rounded-lg shadow-xl animate-fade-in-up">
+      <button className="bg-indigo-600 hover:bg-indigo-800 text-white font-semibold py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-300 ease-in-out" onClick={() => navigate('/submissions')}>
+        🔙 Submission Table
+      </button>
+      <h1 className="text-3xl font-semibold text-center text-gray-800 my-6">Code Submission Form</h1>
+      <div className="max-w-2xl mx-auto bg-white rounded-lg border border-gray-200 shadow-md overflow-hidden">
+        <form onSubmit={handleSubmit} className="p-6">
+          <div className="mb-5">
+            <label htmlFor="username" className="text-sm font-medium text-gray-900 block mb-2">Username:</label>
+            <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
           </div>
-          <div className="mb-4">
-            <label htmlFor="language" className="block text-gray-700 text-sm font-bold mb-2">Preferred Code Language:</label>
-            <select id="language" name="language" value={formData.language} onChange={handleChange} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+          <div className="mb-5">
+            <label htmlFor="language" className="text-sm font-medium text-gray-900 block mb-2">Preferred Code Language:</label>
+            <select id="language" name="language" value={formData.language} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
               <option value="cpp">C++</option>
               <option value="java">Java</option>
               <option value="js">JavaScript</option>
               <option value="python">Python</option>
             </select>
           </div>
-          <div className="mb-4">
-            <label htmlFor="stdin" className="block text-gray-700 text-sm font-bold mb-2">Standard Input:</label>
-            <textarea id="stdin" name="stdin" value={formData.stdin} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="4"></textarea>
+          <div className="mb-5">
+            <label htmlFor="stdin" className="text-sm font-medium text-gray-900 block mb-2">Standard Input:</label>
+            <textarea id="stdin" name="stdin" value={formData.stdin} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" rows="4"></textarea>
           </div>
           <div className="mb-6">
-            <label htmlFor="sourceCode" className="block text-gray-700 text-sm font-bold mb-2">Source Code:</label>
+            <label htmlFor="sourceCode" className="text-sm font-medium text-gray-900 block mb-2">Source Code:</label>
             <AceEditor
               mode={languageModes[formData.language]}
               theme="github"
               onChange={(newCode) => setFormData({ ...formData, sourceCode: newCode })}
               name="sourceCode"
               editorProps={{ $blockScrolling: true }}
-              value={formData.sourceCode}
               setOptions={{
                 enableBasicAutocompletion: true,
                 enableLiveAutocompletion: true,
-                enableSnippets: true,
-                showLineNumbers: true,
-                tabSize: 2,
+                enableSnippets: true
               }}
               className="w-full"
+              height="200px"
             />
           </div>
-          <div className="flex items-center justify-between">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 hover:scale-105" type="submit">
-              Submit Code
-            </button>
-          </div>
+          <button type="submit" className="w-full bg-indigo-600 hover:bg-indigo-800 text-white font-semibold py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-300 ease-in-out">
+            Submit Code
+          </button>
+          {executionResult && (
+            <div className="mt-4 p-4 bg-green-100 text-green-800 rounded-lg">
+              <p className="text-sm">Execution Result:</p>
+              <pre>{executionResult}</pre>
+            </div>
+          )}
+          {executionError && (
+            <div className="mt-4 p-4 bg-red-100 text-red-800 rounded-lg">
+              <p className="text-sm">Execution Error:</p>
+              <pre>{executionError}</pre>
+            </div>
+          )}
         </form>
       </div>
-      {executionResult && (
-        <div className="mt-4 p-4 bg-green-100 border-l-4 border-green-500 text-green-700">
-          <p>Output:</p>
-          <pre>{executionResult}</pre>
-        </div>
-      )}
-      {executionError && (
-        <div className="mt-4 p-4 bg-red-100 border-l-4 border-red-500 text-red-700">
-          <p>Error:</p>
-          <pre>{executionError}</pre>
-        </div>
-      )}
     </div>
   );
 };
